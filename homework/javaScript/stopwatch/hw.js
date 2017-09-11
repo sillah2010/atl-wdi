b0b0893103b187be49c9d3397da13c7d2c33b05a
sillah2010 <macktastic2014@gmail.com>
2017-09-10 23:52:19 -0400

    First half complete

--- homework/javaScript/stopwatch/hw.js
+++ homework/javaScript/stopwatch/hw.js
@@ -3,68 +3,95 @@
 // See LICENSE for details.
 
 /// Data & Core Business Logic ///
-const Stopwatch = {
-  tickClock: function(){
-    if (Stopwatch.isRunning) {
-      setTimeout(Stopwatch.tickClock, 10); // trigger next clock tick
-      Stopwatch.advanceTenMillisecs();
-      AppController.handleClockTick();
+
+$(function () {
+
+  const Stopwatch = {
+    tickClock: function () {
+      if (Stopwatch.isRunning) {
+        setTimeout(Stopwatch.tickClock, 10); // trigger next clock tick
+        Stopwatch.advanceTenMillisecs();
+        AppController.handleClockTick();
+      }
+    },
+    isRunning: false,
+    mins: 0,
+    secs: 0,
+    millisecs: 0,
+    laps: [],
+    // DO NOT EDIT ABOVE THIS LINE
+    advanceTenMillisecs: function () {
+      this.millisecs += 10;
+      if (this.millisecs === 1000) {
+        this.millisecs -= 1000;
+        this.secs += 1;
+      }
+
+      if (this.secs === 60) {
+        this.secs -= 60;
+        this.mins += 1;
+      }
+    },
+    reset: function () {
+      this.mins = 0;
+      this.secs = 0;
+      this.millisecs = 0;
+      this.laps = [];
+    },
+    start: function () {
+      if (!this.isRunning) {
+        this.isRunning = true;
+        this.tickClock();
+        }
+      },
+    stop: function () {
+      if (this.isRunning) {
+        this.isRunning = false;
+      }
+    },
+    lap: function () {
+      if(this.isRunning) {
+        this.laps.push({
+          mins: this.mins,
+          secs: this.secs,
+          millisecs: this.millisecs
+        })
+      }
     }
-  },
-  isRunning: false,
-  mins: 0,
-  secs: 0,
-  millisecs: 0,
-  laps: [],
-  // DO NOT EDIT ABOVE THIS LINE
-  advanceTenMillisecs: function(){
-    // Your Code Here
-  },
-  reset: function(){
-    // Your Code Here
-  },
-  start: function(){
-    // Your Code Here
-  },
-  stop: function(){
-    // Your Code Here
-  },
-  lap: function(){
-    // Your Code Here
-  }
-};
+  };
 
-/// User Interface ///
-const ViewEngine = {
-  updateTimeDisplay: function(mins, secs, millisecs){
-    // Your Code Here
-  },
-  updateLapListDisplay: function(laps){
-    // Your Code Here
-  },
-};
-const ViewHelpers = {
-  zeroFill: function(number, length){
-    // Your Code Here
-  },
-};
+  /// User Interface ///
+  const ViewEngine = {
+    updateTimeDisplay: function (mins, secs, millisecs) {
+      // Your Code Here
+    },
+    updateLapListDisplay: function (laps) {
+      // Your Code Here
+    },
+  };
+  const ViewHelpers = {
+    zeroFill: function (number, length) {
+      // Your Code Here
+    },
+  };
 
-/// Top-Level Application Code ///
-const AppController = {
-  handleClockTick: function(){
-    // Your Code Here
-  },
-  handleClickStart: function() {
-    // Your Code Here
-  },
-  handleClickStopReset: function(){
-    // Your Code Here
-  },
-  handleClickLap: function(){
-    // Your Code Here
-  }
-};
+  /// Top-Level Application Code ///
+  const AppController = {
+    handleClockTick: function () {
+      // Your Code Here
+    },
+    handleClickStart: function () {
+      // Your Code Here
+    },
+    handleClickStopReset: function () {
+      // Your Code Here
+    },
+    handleClickLap: function () {
+      // Your Code Here
+    }
+  };
 
-window.onload = function(){
-  // Attach AppController methods to the DOM as event handlers here.
-};
+  window.onload = function () {
+    // Attach AppController methods to the DOM as event handlers here.
+  };
+})
