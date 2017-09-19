e2f51fb03ea0d439687ce73c40e250889b8d2f52
sillah2010 <macktastic2014@gmail.com>
2017-09-18 23:03:34 -0400

    successful pirate joke

---
+++ homework/node/intro-to-node/pirate_jokes/main.js
@@ -0,0 +1,7 @@
+const notifier = require('node-notifier');
+
+notifier.notify({
+    'title': 'Joke of the Day',
+    'message': 'Q: How much did the pirate pay for his piercings? \nA: A buck-an-ear.'
+  });
+  
