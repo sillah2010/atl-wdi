9f77a61875176336fb24270b32e944a92c4bd5b3
sillah2010 <macktastic2014@gmail.com>
2017-09-19 20:30:04 -0400

    completed GET routes

--- homework/express/pizza-express/app.js
+++ homework/express/pizza-express/app.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(port, function(){
+app.get('/', (req, res) => {
+    res.send("Welcome to Pizza Express!");
+})
+
+app.get('/topping/:type', (req, res) => {
+    res.send(`${req.params.type} pizza! Good choice.`);
+})
+
+app.get('/order/:amount/:size', (req, res) => {
+    res.send(`Your order for ${req.params.amount}  ${req.params.size} pizzas will be ready in 1 minute!!`);
+})
+
+
+app.listen(port, function () {
     console.log("==========================")
     console.log('LISTENING ON PORT ' + port);
     console.log("==========================")
-  });
+});
