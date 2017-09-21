99c09d6bd5df7d3127dba302d4a3748d33e7a245
sillah2010 <macktastic2014@gmail.com>
2017-09-20 22:55:30 -0400

    Create and show completed

--- homework/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,23 @@
-//your code below
+const express = require("express");
+const router = express.Router();
+const pirates = require('../models/pirates.js');
+
+router.get("/", (req,res) => {
+    res.render('pirates/index', {
+        pirates: pirates 
+    });    
+})
+
+router.get('/new', (req, res) => {
+    res.render('pirates/new')
+})
+
+router.get('/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const pirate = pirates[id];
+      res.render('pirates/show', {
+          pirate: pirate
+    })
+})
+
+module.exports = router;
--- homework/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,17 @@
-//your code here
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const bodyParser = require('body-parser');
+const pirateController = require('./controllers/pirates.js');
+
+app.use("/pirates", pirateController);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'hbs');
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("express is listening on port ", port);
+})
