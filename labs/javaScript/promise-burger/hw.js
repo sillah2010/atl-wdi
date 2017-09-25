4bd9129feb21b391e5459ed9216aa430cae085b1
sillah2010 <macktastic2014@gmail.com>
2017-09-24 20:45:02 -0400

    promise burger done

--- labs/javaScript/promise-burger/hw.js
+++ labs/javaScript/promise-burger/hw.js
@@ -6,8 +6,8 @@ const kitchen = require('./kitchen');
 // 1. Kitchen has a method called `.readMenu`.  
 // Call this method and console.log the data that comes back when the Promise is resolved.
 
-//CODE FOR QUESTION 1 HERE
-
+kitchen.readMenu().then(console.log).catch(console.log);
+  
 
 // Order Some Food
 /* *************************************************************** */
@@ -15,14 +15,13 @@ const kitchen = require('./kitchen');
 //  The order method has one argument, the name of the burger you would like to order.  
 //  Console.log the data that comes back about this burger.
 
-// CODE FOR QUESTION 2 HERE
-
+kitchen.order('Yokohama Mama').then(console.log);
 
 // 3. Try and use the same method to order a `quesadilla`.  
 //  Since Promise Burger only serves burgers, this will throw an error.  
 //  Make sure that your Promise can be dealt with when it is rejected.
 
-// CODE FOR QUESTION 3 HERE
+kitchen.order('Chimichanga').then(console.log).catch(console.log);
 
 
 // Add A Burger To The Menu
@@ -34,17 +33,20 @@ const kitchen = require('./kitchen');
 //  Add a method to console.log the response once the promise is resolved!
 //  Also add a method to catch an error if something goes wrong
 
-const newBurger = {
-  "name": "TRIPLE CORONARY BYPASS",
-  "price": 28.95,
-  "description": ` It becomes difficult to even describe this, the most preposterous of our Bypass Burgers. All you really need to know is that we use three burger patties, three fried eggs, fourteen slices of American cheese, and ten slices of bacon, all packed between two grilled cheese sandwiches. Figuring out how to add condiments is completely up to you. It’s served in a big bowl of french fries and tater tots covered, in lots of our Cheesy-Cheese Goo. Enjoy!`
-}
+ const newBurger = {
+   "name": "TRIPLE CORONARY BYPASS",
+   "price": 28.95,
+   "description": ` It becomes difficult to even describe this, the most preposterous of our Bypass Burgers. All you really need to know is that we use three burger patties, three fried eggs, fourteen slices of American cheese, and ten slices of bacon, all packed between two grilled cheese sandwiches. Figuring out how to add condiments is completely up to you. It’s served in a big bowl of french fries and tater tots covered, in lots of our Cheesy-Cheese Goo. Enjoy!`
+ }
+ 
+ kitchen.addToMenu(newBurger).then(console.log).catch(console.log).then(kitchen.readMenu().then(console.log))
 
-// CODE FOR QUESTION 4 HERE
 
 // 5. Validate that the new item has been added to the menu by calling `readMenu` again.  
 // We only want to call `readMenu` after we get a response that is successful. 
 // Remember that we can chain promises together the same way that we can chain jQuery methods.
+
+
 
 // REFACTOR QUESTION 4 TO ACCOUNT FOR THIS REQUIREMENT
 
@@ -60,17 +62,23 @@ const newBurger = {
 const customers = [
   {
     name: "Josie",
-    order: "Holy Guacamole"
+    order: "Holy Guacamole",
+    
   }, {
     name: "Chris",
-    order: "Big Blue Buffalo"
+    order: "Big Blue Buffalo",
+
   }, {
     name: "John",
     order: "Blue 'Shroom",
+  
   }, {
     name: "Katelyn",
-    order: "Fat Elvis"
+    order: "Fat Elvis",
+
   }
 ]
 
-// CODE FOR QUESTION 6 HERE
+Promise.all(customers.map((people) => {
+  kitchen.order(people.order).then(console.log).catch(console.log);
+}))
