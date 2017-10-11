d3385dc49c74d9956990a2a2ce6d1931a8f4c584
sillah2010 <macktastic2014@gmail.com>
2017-10-11 18:22:59 -0400

    Finished HW

--- homework/react/bootflix/src/App.js
+++ homework/react/bootflix/src/App.js
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Header from './components/Header';
 import Search from './components/Search';
 import Movie from './components/Movie';
-import example from './omdbExample.json'
+//import example from './omdbExample.json'
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      movie: example
+      movie: {},
+      id: "",
+      title: ""
+      //movieObject: {
+      //title: '',
+
     }
   }
 
   //Update these methods to make axios calls to OMDB and update this.state.movie with the response from the server
-  _searchByTitle = () => {
-    console.log("Search by Title");
+  searchByTitle = (event) => {
+    event.preventDefault()
+    axios.get(`http://www.omdbapi.com/?apikey=d31f1a94&t=${this.state.title}`)
+      .then((movie) => {
+        this.setState({
+          movie: movie.data
+        })
+        console.log(movie);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
-  _searchById = () => {
-    console.log("Search by ID");
+  searchById = (event) => {
+    event.preventDefault()
+    axios.get(`http://www.omdbapi.com/?apikey=d31f1a94&i=${this.state.id}`)
+      .then((movie) => {
+        this.setState({
+          movie: movie.data
+        })
+        console.log(movie.data);
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  handleChange = (event) => {
+    const newState = { ...this.state };
+    newState[event.target.name] = event.target.value;
+    this.setState(newState);
   }
 
   //Pass _searchByTitle, _searchById, and this.state.movie to it's appropriate child components.
@@ -26,8 +58,13 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Search />
-        <Movie />
+        <Search
+          searchByTitle={this.searchByTitle}
+          searchById={this.searchById}
+          handleChange={this.handleChange}
+        />
+        <Movie
+          movie={this.state.movie} />
       </div>
     );
   }
--- homework/react/bootflix/src/components/Search.jsx
+++ homework/react/bootflix/src/components/Search.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 // Update the forms to utilize methods being passed down from App.js
-const Search = () => {
+const Search = (props) => {
   return (
     <section id="movie-search">
       <strong>Search by:</strong> Title <em>or</em> ID
 
       <div className="search">
-        <form id="title-search-form" method="get">
-          <input type="text" name="title" placeholder="Enter movie title" />
+        <form onSubmit={props.searchByTitle} id="title-search-form" method="get">
+          <input type="text" name="title" placeholder="Enter movie title" onChange={props.handleChange}/>
           <input type="submit" value="Search for this title" />
         </form>
       </div>
 
       <div className="search">
-        <form id="id-search-form" method="get">
-          <input type="text" name="id" placeholder="Enter omdb movie ID" />
+        <form onSubmit={props.searchById} id="id-search-form" method="get">
+          <input type="text" name="id" placeholder="Enter omdb movie ID"  onChange={props.handleChange}/>
           <input type="submit" value="Search by ID" />
         </form>
       </div>
