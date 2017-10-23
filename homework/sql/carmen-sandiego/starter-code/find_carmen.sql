9e8529cd1e0800e6fd57f701c256b632bbabfc0a
sillah2010 <macktastic2014@gmail.com>
2017-10-23 06:19:14 -0400

    CARMEN SANDIEGO - I found Carmen Sandiego

--- homework/sql/carmen-sandiego/starter-code/find_carmen.sql
+++ homework/sql/carmen-sandiego/starter-code/find_carmen.sql
@@ -2,5 +2,9 @@
  2) SELECT language FROM countrylanguage WHERE countrycode = 'VAT'; (Italian)
  3) SELECT * FROM countrylanguage WHERE language = 'Italian' AND percentage = 100;
     SELECT * FROM country WHERE code = 'SMR'; (San Marino)
- 4) SELECT * FROM city WHERE countrycode = 'SMR'; (Serravalle) 
+ 4) SELECT * FROM city WHERE countrycode = 'SMR'; (Serravalle)
+ 5) SELECT * FROM city WHERE name LIKE 'Serra%';
+    SELECT * FROM country WHERE code='BRA';  (Serra, Brazil)
+ 6) SELECT * FROM city WHERE id='211'; (Brasilia)
+ 7) SELECT name, district, countrycode FROM city WHERE population = 91084; (Santa Monica, California)
  
