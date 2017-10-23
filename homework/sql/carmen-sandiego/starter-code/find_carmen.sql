c9d29f2edebc2d33576980c9f4f82d57dbd98f8d
sillah2010 <macktastic2014@gmail.com>
2017-10-23 06:10:21 -0400

    CARMEN SANDIEGO - Halfway there! Hot on her trail.

---
+++ homework/sql/carmen-sandiego/starter-code/find_carmen.sql
@@ -0,0 +1,6 @@
+ 1) SELECT name, region, population FROM country WHERE region='Southern Europe' ORDER BY population; Holy See (Vatican City State)
+ 2) SELECT language FROM countrylanguage WHERE countrycode = 'VAT'; (Italian)
+ 3) SELECT * FROM countrylanguage WHERE language = 'Italian' AND percentage = 100;
+    SELECT * FROM country WHERE code = 'SMR'; (San Marino)
+ 4) SELECT * FROM city WHERE countrycode = 'SMR'; (Serravalle) 
+ 
--- labs/sql/library_sql/advanced_queries.sql
+++ labs/sql/library_sql/advanced_queries.sql
@@ -2,17 +2,24 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
+SELECT * FROM authors
+JOIN books ON authors.id = books.author_id
 
 -- Find all fields (book and author related) for all books written by Milan Kundera.
 
 -- Find all books written by an author from China or the UK.
-
+SELECT * FROM books
+INNER JOIN authors ON books.author_id = authors.id
+WHERE authors.nationality IN ('China', 'United Kingdom');
 -- Find out how many books Albert Camus wrote.
 
 -- Find out how many books were written by US authors.
 
 -- Find all books written after 1930 by authors from Argentina.
-
+SELECT * FROM authors
+JOIN books ON authors.id = books.author_id
+WHERE authors.nationality = 'Argentina'
+AND books.publication_date > 1930;
 -- Find all books written before 1980 by authors not from the US.
 
 -- Find all authors whose names start with 'J'.
