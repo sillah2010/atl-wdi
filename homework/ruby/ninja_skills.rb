37d28b8eab6f288ee6846f097c8e20efddf45c0f
sillah2010 <macktastic2014@gmail.com>
2017-10-23 16:46:32 -0400

    2.  Nil Skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,10 +1,14 @@
 #Challenge 1
 a =  true
 b = false
-
 puts a = b
-puts a != b
+puts a = !b
 
+#Challenge 2
+dora = nil
+p dora
+puts "#{dora} nil was here"
+print dora
+puts dora.class
 
-
-
+#Challenge 3
