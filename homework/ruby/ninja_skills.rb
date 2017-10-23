e30bba448b038332739545f45a29787d6918d056
sillah2010 <macktastic2014@gmail.com>
2017-10-23 17:17:45 -0400

    6. Array Skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -29,12 +29,22 @@ gToNumber = 2
 puts g. + gToNumber.to_s
 
 #Challenge 5
-puts "Input an integer"
-integer = gets.chomp.to_i
-puts integer * 2
+#puts "Input an integer"
+#integer = gets.chomp.to_i
+#puts integer * 2
+#
+#puts "What is your name"
+#name = gets.chomp
+#puts "What do you like to do"
+#likesToDo = gets.chomp
+#puts "#{name} likes to #{likesToDo}"
 
-puts "What is your name"
-name = gets.chomp
-puts "What do you like to do"
-likesToDo = gets.chomp
-puts "#{name} likes to #{likesToDo}"
+#Challenge 6
+myFirstArray = []
+myFirstArray = ["Jonathan", "Joseph", "Jotaro", "Josuke", "Giorno", 4, 5, 6, 7]
+puts myFirstArray.last
+puts myFirstArray.first
+myFirstArray[1] = "Joe"
+myFirstArray[2] = 3
+myFirstArray.push(true)
+puts myFirstArray[3].class
